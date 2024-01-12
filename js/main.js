bc8a43374b440313058f2661eec7f843850691ba
jessica mier <jessi.m@live.com.ar>
2024-01-12 01:12:14 -0300

    iniciando proyecto

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const divDestinos= document.getElementById("divDestinos")
+fetch('./json/paquetes.json')
+.then(response => response.json())
+.then(destinos =>{
+    destinos.forEach((destino, indice) => {
+        divDestinos.innerHTML +=`
+        <div id="destino${indice}" class="card lugares m-2 bg-body-secondary" style="width: 18rem;">
+            <img src="./img/${destino.img}" class="card-img-top imgCard" alt="...">
+            <div class="card-body text-center">
+                <h5 class="card-title">${destino.destino}</h5>
+                <p class="card-text">Origen: ${destino.origen}</p>
+                <p class="card-text">Salida: ${destino.salida}</p>
+                <p class="card-text">Hotel: ${destino.hotel}</p>
+                <p class="card-text">Noches: ${destino.noches}</p>
+                <p class="card-text">Precio: ${destino.precio}</p>
+                <button class =" btn btn-primary">Comprar</button>
+            </div>
+            </div>
+            </div>
+        </div>`
+    });
+})
+
+async function mostrarDestinos() {
+    const destinos = await fetch("./json/paquetes.json")
+    const destinosParseados= await destinos.json()
+    console.log(destinosParseados)
+    return destinosParseados
+}
+mostrarDestinos()
